a3dae7b4f66c109662624adf542bea4ebce66935
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-02 23:10:35 +0530

    utils folder setup & database setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import listingRouter from "./routes/listing.routes.js";
 dotenv.config();
 
 const app = express();
@@ -7,5 +8,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Server is connected!!");
 });
+
+app.use("/api/v1/listings", listingRouter);
 
 export default app;
