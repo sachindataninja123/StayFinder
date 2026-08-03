eeddb6e90cdc561e9e68040747b15667e809e992
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-03 17:07:21 +0530

    cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import listingRouter from "./routes/listing.routes.js";
 dotenv.config();
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.get("/", (req, res) => {
   res.send("Server is connected!!");
 });
