4d775c227f8150dde1f9b2ecf4a48e20aaaca4aa
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-08-11 23:03:23 +0530

    protected route implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Listing from "./pages/Listing";
 import { PropertyDetailsPage } from "./pages/property/PropertyDetailsPage";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
@@ -10,6 +9,7 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import { useDispatch } from "react-redux";
 import { getMeUser } from "./features/auth/auth.slice";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -24,14 +24,16 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/listing" element={<Listing />} />
-        <Route path="/listings/:id" element={<PropertyDetailsPage />} />
-        <Route path="/listings/new" element={<CreateListing />} />
-        <Route path="/listings/:id/edit" element={<EditListing />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/signin" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/listings/:id" element={<PropertyDetailsPage />} />
+          <Route path="/listings/new" element={<CreateListing />} />
+          <Route path="/listings/:id/edit" element={<EditListing />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/signin" state={{ from: location }} replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
